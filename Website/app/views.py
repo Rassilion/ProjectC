e1ab36befd674a34e012d8e1d1b676eaafd1ebed
Rassilion <denizgezgin365@gmail.com>
2015-06-15 00:44:36 +0300

    solution page

--- Website/app/views.py
+++ Website/app/views.py
@@ -51,8 +51,8 @@ def problem(slug):
 @app.route('/problem/<slug>/solution')
 @login_required
 def problem_solution(slug):
-    solution = Problems.query.filter_by(slug=slug).first()
-    return render_template('problem.html', title=solution.title, solution=solution)
+    problem = Problems.query.filter_by(slug=slug).first()
+    return render_template('problem_solution.html', title=problem.title, problem=problem)
 
 @app.route('/problem/<slug>/suggestion')
 @login_required
