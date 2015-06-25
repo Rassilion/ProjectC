e846378b079b1f36f6706b50445f55e5b11d96d7
Rassilion <denizgezgin365@gmail.com>
2015-06-25 20:44:38 +0300

    added Problem model form

--- Website/app/forms.py
+++ Website/app/forms.py
@@ -1,7 +1,24 @@
 # !/usr/bin/python
-# -*- coding: utf-8 -*-from flask.ext.wtf import Form
-from flask_security.forms import RegisterForm, Required, TextField
+# -*- coding: utf-8 -*-
+from flask_security.forms import RegisterForm, Required
+from flask_wtf import Form
+from wtforms import TextField
+from wtforms.ext.sqlalchemy.fields import QuerySelectMultipleField
+from models import Tag, Problem
+from wtforms_alchemy import ModelForm
 
 
 class ExtendedRegisterForm(RegisterForm):
     username = TextField('Username', [Required()])
+
+
+def tags():
+    return Tag.query.order_by(Tag.name).all()
+
+
+class ProblemForm(ModelForm, Form):
+    class Meta:
+        model = Problem
+        only = ['title', 'body', 'solution', 'difficulty']
+
+    tags = QuerySelectMultipleField(u'İlgili konular', query_factory=tags)
