39245f8420b9fa4c98fd61f2a5ee825fdbb721c4
Rassilion <denizgezgin365@gmail.com>
2015-08-28 02:26:31 +0300

    Fix

--- website/utils/add_attribute.py
+++ website/utils/add_attribute.py
@@ -1,7 +1,7 @@
 from __future__ import absolute_import
 from __future__ import unicode_literals
 from markdown.treeprocessors import Treeprocessor
-from markdown.extensions import Extension
+from markdown import Extension
 
 
 class AddAttributeTreeprocessor(Treeprocessor):
