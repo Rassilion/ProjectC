5a18d50d3b59e44bbbc2eff995ba39620ff27fcc
Rassilion <denizgezgin365@gmail.com>
2017-07-31 12:35:50 +0300

    clean code

--- Problems/003/generator.py
+++
@@ -1,5 +0,0 @@
-from random import randint
-
-for i in range(1, 101):
-    asd = open("inp/" + str(i), "w")
-    asd.write(str(randint(1, 999999999)) + "\n")
--- Problems/004/generator.py
+++
@@ -1,5 +0,0 @@
-for i in range(1, 47):
-    asd = open("inp/" + str(i), "w")
-    s = str(i - 1) + "\n"
-    asd.write(s)
-    asd.close
